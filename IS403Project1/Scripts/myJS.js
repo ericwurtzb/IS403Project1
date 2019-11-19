28381637c723ce7468d531257db6f3e52371b27a
Aaron Hayden <aaronjhayden@gmail.com>
2019-11-19 16:03:57 -0700

    Javascript

---
+++ IS403Project1/Scripts/myJS.js
@@ -0,0 +1,21 @@
+﻿var answer = [];
+var counter = [];
+
+for (iCount = 0; iCount < 10; iCount++) {
+    counter[iCount] = 1;
+    answer[iCount] = "";
+}
+
+
+function response(num) {
+    document.getElementById("btn" + num).style.display = "none";
+    var newForm = "<form action='SubmitForm' method='post'><textarea id='textarea" + num + "' name='answer' + placeholder='Respond here...'></textarea> <br /> <input type='button' onclick='submitAnswer(" + num + ")' value='Save' /></form>";
+    document.getElementById("output" + num).innerHTML = newForm;
+}
+
+function submitAnswer(num) {
+    document.getElementById("btn" + num).style.display = "inline-block";
+    answer[parseInt(num)] += "Answer " + counter[parseInt(num)] + ": " + document.getElementById("textarea" + num).innerHTML + "<br>";
+    document.getElementById("output" + num).innerHTML = answer[parseInt(num)];
+    counter[parseInt(num)]++;
+}
