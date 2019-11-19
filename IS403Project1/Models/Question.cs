5aee3fb9d04a27d76e4737fb82beacdf67681f92
Ryan Ham <ryanham@byu.edu>
2019-11-19 15:11:03 -0700

    question model

---
+++ IS403Project1/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace IS403Project1.Models
+{
+    public class Question
+    {
+        [DisplayName("Question Code")]
+        public int question_Code { get; set; }
+        [DisplayName("Question Name")]
+        public string question_Name { get; set; }
+    }
+}
