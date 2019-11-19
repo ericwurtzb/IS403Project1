3b85d13c8797b6fcc7ff6cee116c8d2d9ed2415a
Ryan Ham <ryanham@byu.edu>
2019-11-19 15:00:01 -0700

    changes

---
+++ IS403Project1/Controllers/MissionsController.cs
@@ -0,0 +1,20 @@
+﻿using MissionProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS403Project1.Controllers
+{
+    public class MissionsController : Controller
+    {
+        public static List<Mission> lstMissions = new List<Mission>();
+    
+        // GET: Missions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
